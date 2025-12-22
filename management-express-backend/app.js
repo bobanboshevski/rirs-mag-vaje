a71f0562aceab1f61493bf2eaf25f06a3ce32a58
bobanboshevski <152434305+bobanboshevski@users.noreply.github.com>
2025-12-22 23:33:55 +0100

    fixed allowed origins

--- management-express-backend/app.js
+++ management-express-backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://rirs-backend-dev.onrender.com'],
+    origin: ['http://localhost:3000', 'https://rirs-frontend-dev.onrender.com'],
 }));
 app.use(express.json());
 app.use(morgan('dev'));
